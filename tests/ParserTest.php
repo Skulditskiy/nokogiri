0c5c7610ea0819ea79b9b145a5be6c6b40c8b0ce
yevgeniy.skulditskiy <yevgeniy.skulditskiy@digitalperformance.de>
2018-11-14 08:53:27 +0100

    Simpliest possible test added

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -18,4 +18,22 @@ class ParserTest extends TestCase
         // verify
         $this->assertInstanceOf(Parser::class, $classUnderTest);
     }
+
+    /**
+     * @test
+     */
+    public function loadHtml_readingContentOfBasicElement_test()
+    {
+        // prepare
+        $classUnderTest = new Parser();
+        $expectedResult = 'link';
+
+        // test
+        $classUnderTest->loadHtml('<div><a>' . $expectedResult . '</a><b>hello world</b></div>');
+        $result = $classUnderTest->get('div a')->toArray()[0]['#text'][0];
+
+        // verify
+        $this->assertEquals($expectedResult, $result);
+    }
+
 }
